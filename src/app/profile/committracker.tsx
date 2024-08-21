9d564b57ef58f0d0b71e36bbc6efa3d968ada521
Nils Hansson <nilsmenghansson@gmail.com>
2024-08-21 15:17:11 +0200

    daisy for committracker
    Co-authored-by: Otto Bossmann <otto.kostmann@hotmail.com>

--- src/app/profile/committracker.tsx
+++ src/app/profile/committracker.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import { SelectStudent } from "@/db/query";
 import { useState, useEffect } from "react";
 const GITHUB_TOKEN = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
 
-interface GHCommitProps{
-  student:SelectStudent
+interface GHCommitProps {
+  student: SelectStudent;
 }
 
 const GitHubCommits = ({ student }: GHCommitProps) => {
@@ -92,11 +92,28 @@ const GitHubCommits = ({ student }: GHCommitProps) => {
   }, [student.github]);
 
   if (loading) {
-    return <div>Loading GitHub commits...</div>;
+    return <span className="loading loading-spinner loading-sm"></span>;
   }
 
   if (error) {
-    return <div>{error}</div>;
+    return (
+      <div role="alert" className="alert alert-error">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>{error}</span>
+      </div>
+    );
   }
 
   return (
