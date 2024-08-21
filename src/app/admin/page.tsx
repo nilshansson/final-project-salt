dc31f3bfa5dbd520f3abf9e186a287287338602d
Nils Hansson <nilsmenghansson@gmail.com>
2024-08-21 15:43:53 +0200

    small fix again for studentcard
    Co-authored-by: Otto Bossmann <otto.kostmann@hotmail.com>

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -29,7 +29,7 @@ export default function AdminPage() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 gap-14 sm:grid-cols-2 lg:grid-cols-4 p-4">
+    <div className="grid grid-cols-1 gap-14 sm:grid-cols-2 lg:grid-cols-4 p-4 justify-items-center">
       {studentsInfo.map((student) => (
         <StudentCard key={student.userId} student={student} />
       ))}
