6e091d7fb9edfedd715739d16d96479d00778bec
Otto Bossmann <otto.kostmann@hotmail.com>
2024-08-23 15:11:07 +0200

    favico
    Co-authored-by: nilshansson <nilsmenghansson@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png"/>
+          <link rel="icon" href="/favicon-64x64.png" sizes="64x64" type="image/png"/>
+        </head>
         <body className={manrope.className}>
           <Navbar />
           {children}
