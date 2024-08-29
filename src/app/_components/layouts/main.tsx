0a9009e3e0f6c976c0233ed08aeabc7b321f2175
Otto Bossmann <otto.kostmann@hotmail.com>
2024-08-29 13:55:29 +0200

    admin dashboard Main
    Co-authored-by: nilshansson <nilsmenghansson@gmail.com>

--- src/app/_components/layouts/main.tsx
+++ src/app/_components/layouts/main.tsx
@@ -8,7 +8,7 @@ export function Main({ children, title }: Props) {
     <main className="max-w-full mx-auto flex flex-col drop-shadow-2xl">
       <div className="p-5">
         <div className="p-8 w-full bg-content text-text drop-shadow-2xl rounded">
-        <div className="card p-4 bg-white drop-shadow-2xl">
+        <div className="card p-4 h-auto w-auto bg-white drop-shadow-2xl">
           <h1 className="text-center text-5xl font-extrabold text-saltDarkBlue tracking-tighter py-10">
             {title}
           </h1>
