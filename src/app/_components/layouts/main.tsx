53582a3b44a55e667a60fe9a394f22e3e79fabd9
Otto Bossmann <otto.kostmann@hotmail.com>
2024-08-29 13:47:50 +0200

    main card added
    Co-authored-by: nilshansson <nilsmenghansson@gmail.com>

--- src/app/_components/layouts/main.tsx
+++ src/app/_components/layouts/main.tsx
@@ -8,10 +8,12 @@ export function Main({ children, title }: Props) {
     <main className="max-w-full mx-auto flex flex-col drop-shadow-2xl">
       <div className="p-5">
         <div className="p-8 w-full bg-content text-text drop-shadow-2xl rounded">
+        <div className="card p-4 bg-white drop-shadow-2xl">
           <h1 className="text-center text-5xl font-extrabold text-saltDarkBlue tracking-tighter py-10">
             {title}
           </h1>
           {children}
+          </div>
         </div>
       </div>
     </main>
